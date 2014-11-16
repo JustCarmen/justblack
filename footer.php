856c6c69fc04bcee4f0d59fc2519ac65b3885684
JustCarmen <carmen@justcarmen.nl>
2014-11-16 05:38:31 +0100

    Error in footer file due to changes in webtrees-base.
    See: https://github.com/fisharebest/webtrees/commit/478e4aeec46fe090c0bed8427f3d64b8884f2dac

--- footer.php
+++ footer.php
@@ -36,7 +36,7 @@ if ($view!='simple') {
 	echo '<a href="', WT_WEBTREES_URL, '" target="_blank" class="icon-webtrees" title="', WT_WEBTREES, ' ', WT_VERSION, '"></a>';
 	echo '<br><a href="http://www.justcarmen.nl" target="_blank">Design: justcarmen.nl</a>';
 	echo '</p>';
-	if (WT_DEBUG || $WT_TREE->getPreference('SHOW_STATS')) {
+	if ($WT_TREE && $WT_TREE->getPreference('SHOW_STATS')) {
 		echo execution_stats();
 	}
 	if (exists_pending_change()) {
