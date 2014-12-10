982d20ef3ccf9c04962d621c9739b324b22c4037
JustCarmen <carmen@justcarmen.nl>
2014-12-10 22:13:03 +0100

    Set position fixed on all objects.

--- colorbox-1.5.14/justblack.colorbox.js
+++ colorbox-1.5.14/justblack.colorbox.js
@@ -2,17 +2,17 @@
  *  webtrees: Web based Family History software
  *  Copyright (C) 2014 webtrees development team.
  *  Copyright (C) 2014 JustCarmen.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
@@ -78,7 +78,8 @@ jQuery("body").one('click', 'a.gallery', function(event) {
 		current:		"",
 		slideshow:		true,
 		slideshowAuto:	false,
-		slideshowSpeed: 3000
+		slideshowSpeed: 3000,
+		fixed:			true
 	});
 
 	// Image settings
@@ -100,7 +101,7 @@ jQuery("body").one('click', 'a.gallery', function(event) {
 								resizeImg();
 							}
 							jQuery(".cboxPhoto").wheelzoom();
-							jQuery(".cboxPhoto img").on("click", function(e) {e.preventDefault();});								
+							jQuery(".cboxPhoto img").on("click", function(e) {e.preventDefault();});
 							longTitles();
 						}
 	});
@@ -109,7 +110,6 @@ jQuery("body").one('click', 'a.gallery', function(event) {
 	jQuery("a[type$=pdf].gallery").colorbox({
 		width:		"75%",
 		height:		"90%",
-		fixed:		true,
 		iframe:		true,
 		title:		function(){
 						var pdf_title = jQuery(this).data("title");
