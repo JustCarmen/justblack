d46e0f6eab2b6dc251ff34bd6e79eceafbd8b7d8
JustCarmen <carmen@justcarmen.nl>
2015-06-03 00:23:43 +0200

    PSR4: Namespaces - following webtrees base.

--- theme.php
+++ theme.php
@@ -19,6 +19,10 @@ namespace Fisharebest\Webtrees;
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Menu;
+use Fisharebest\Webtrees\Theme\BaseTheme;
+
 class JustBlackTheme extends BaseTheme {
 
 	/** @var string the location of this theme */
