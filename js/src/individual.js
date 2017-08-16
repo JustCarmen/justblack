fb71593b9767ff32a9a0b4a6b4520be4a4c9fe64
JustCarmen <carmen@justcarmen.nl>
2017-08-16 07:23:03 +0200

    Grid and individual page js

---
+++ js/src/individual.js
@@ -0,0 +1,12 @@
+// Scripts for the individual page
+$('.jc-global-individual').each(function() {
+  var html = $('h2').html();
+  var parts = html.split(',');
+  $('h2').html(parts[0]);
+  $('.wt-main-container .card:first .card-header').each(function() {
+    $(this)
+      .addClass('d-flex flex-column flex-lg-row justify-content-lg-between')
+      .wrapInner('<div class="jc-individual-name">')
+      .append('<div class="jc-individual-age">' + parts[1]);
+  });
+});
